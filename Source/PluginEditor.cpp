d90375fdb63b6bc2b0895b1e93f4606d63785382
emmettq2 <emmettq2@illinois.edu>
2025-02-19 00:50:27 -0600

    declared a few buttons
    Please enter the commit message for your changes. Lines starting

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,17 @@ VocoderAudioProcessorEditor::VocoderAudioProcessorEditor (VocoderAudioProcessor&
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+    
+    // implement your slider/buttons here
+    lowFreq.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    lowFreq.setRange(0.0, 127.0, 0.5);
+    lowFreq.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    lowFreq.setPopupDisplayEnabled (true, false, this);
+    lowFreq.setTextValueSuffix ("Lo");
+    lowFreq.setValue(1.0);
+    
+    // add the slider/button to the gui
+    addAndMakeVisible(&lowFreq);
 }
 
 VocoderAudioProcessorEditor::~VocoderAudioProcessorEditor()
@@ -30,11 +41,16 @@ void VocoderAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    
+    //write fun stuff
+    g.drawFittedText ("hi guys", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void VocoderAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    // sets the position and size of the slider with arguments (x, y, width, height)
+    lowFreq.setBounds(0, 0, 200, 200);
 }
