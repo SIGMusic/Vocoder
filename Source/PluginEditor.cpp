d3b6561e320468ce57678a439155e1a7478c02f2
dkapadia1 <44717095+dkapadia1@users.noreply.github.com>
2025-03-14 00:25:57 -0500

    Vocoder box added, scaling works smoothly. Vocoder number only updates on resize. Very confused on why it doesn't work on start.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,9 +15,9 @@ VocoderAudioProcessorEditor::VocoderAudioProcessorEditor (VocoderAudioProcessor&
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (1000, 600);
+    setSize (width_, height_);
     setResizable(true, true);
-    getConstrainer()->setFixedAspectRatio(1000.0/600.0);
+    getConstrainer()->setFixedAspectRatio(ratio_);
     
     iGain.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
     iGain.setRange(-12.0, 6.0, 0.1);
@@ -33,7 +33,13 @@ VocoderAudioProcessorEditor::VocoderAudioProcessorEditor (VocoderAudioProcessor&
     nBands.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 10);
     nBands.setPopupDisplayEnabled (false, false, this);
     // frequency ranges 20-12000 (setup later)
-    
+    for (auto& c : vocoders) {
+        c.setSliderStyle(juce::Slider::LinearVertical);
+        c.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    }
+    for (int i = 0; i < 8; i++) {
+        addAndMakeVisible(&vocoders[i]);
+    }
     // add the slider/button to the gui
 //    addAndMakeVisible(&attack);
 //    addAndMakeVisible(&release);
@@ -52,14 +58,14 @@ void VocoderAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
+    g.setColour (juce::Colours::darkblue);
     g.setFont (juce::FontOptions (15.0f));
+    float scalar = static_cast<float>(getHeight()) / height_;
+    g.fillRect(200 * scalar, 100 * scalar, (getWidth()) - (250 * scalar), (getHeight()) - (200 * scalar));
     
-    g.fillRect(100, 100, 100 ,100);
     
-    
-    //write fun stuff
-    g.drawFittedText ("hi guys\nadding this line to test debugging", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
+    //write fun stuff "hi guys\nadding this line to test debugging12" +
+    g.drawFittedText (std::to_string(nBands.getValue()), 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void VocoderAudioProcessorEditor::resized()
@@ -68,11 +74,25 @@ void VocoderAudioProcessorEditor::resized()
     // subcomponents in your editor..
     
     // setBounds sets the position and size of the slider with arguments (x, y, width, height)
-    iGain.setBounds(0, 50, 50, 50);
-    oGain.setBounds(50, 50, 50, 50);
-    nBands.setBounds(150, 0, 100, 100);
+    float vocoderWidth = 750.0 / nBands.getValue();
+    for (int i = 0; i < nBands.getValue(); i++) {
+        addAndMakeVisible(&vocoders[i]);
+        setComponent(vocoders[i], i * vocoderWidth + 200, 100, vocoderWidth, 400);
+    }
+    runs++;
+    setComponent(iGain, 0, 50, 100, 100);
+    setComponent(oGain, 100, 50, 100, 100);
+    //oGain.setBounds(50, 50, 50, 50);
+    setComponent(nBands, 0, 250, 200, 200);
+    //nBands.setBounds(150, 0, 100, 100);
 }
-void VocoderAudioProcessorEditor::setComponent(juce::Component c, int left, int top, int width, int height) {
-
+void VocoderAudioProcessorEditor::setComponent(juce::Component& c, int left, int top, int width, int height) {
+    //use ratio to make it ratioed
+    double scalar = (static_cast<double>(getHeight()) / height_);
+    int rleft = scalar * left;
+    int rtop = scalar * top;
+    int rwidth = scalar * width;
+    int rheight = scalar * height;
+    c.setBounds(rleft, rtop, rwidth, rheight);
         
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,7 +23,7 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
-    void setComponent(juce::Component c, int left, int top, int width, int height);
+    void setComponent(juce::Component& c, int left, int top, int width, int height);
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
@@ -32,10 +32,14 @@ private:
     //define the buttons and sliders and such here first
 //    juce::Slider attack;
 //    juce::Slider release;
-    
+    const int height_ = 600;
+    const int width_ = 1000;
+    const float ratio_ = 1000.0 / 600.0;
+    const int maxVocoders = 40;
     juce::Slider iGain;
     juce::Slider oGain;
     juce::Slider nBands;
-    
+    std::vector<juce::Slider> vocoders = std::vector<juce::Slider> (maxVocoders);
+    int runs = 0;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VocoderAudioProcessorEditor)
 };
