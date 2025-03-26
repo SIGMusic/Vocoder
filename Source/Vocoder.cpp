e4625dec285bb8311df839f25962511858ba8486
uiobio <lishenglei2@gmail.com>
2025-03-25 19:40:14 -0500

    Testing filterBank - to replicate in AudioPluginHost, send a signal through the sidechain (blue nodes)

--- Source/Vocoder.cpp
+++ Source/Vocoder.cpp
@@ -3,7 +3,7 @@
 void Vocoder::prepare(const juce::dsp::ProcessSpec& spec)
 {
     const std::vector<float> centerFrequencies = { 100.0f, 195.0f, 380.25f, 741.49f, 1445.90f, 2819.51f, 5498.04f, 10721.17f };
-    const std::vector<float> Qs(centerFrequencies.size(), 1.0f); // Q = 1.0 for all bands
+    const std::vector<float> Qs(centerFrequencies.size(), 50.0f); // Q = 1.0 for all bands
 
     filterBank_.prepare(spec.sampleRate, spec.maximumBlockSize, spec.numChannels, numBands_, centerFrequencies, Qs);
     modulator_.prepare(spec.sampleRate);
@@ -12,15 +12,32 @@ void Vocoder::prepare(const juce::dsp::ProcessSpec& spec)
 void Vocoder::process(juce::AudioBuffer<float>& carrier, juce::AudioBuffer<float>& modulator, juce::AudioBuffer<float>& output)
 {
     filterBank_.process(carrier, carrierBands);
-    filterBank_.process(modulator, modulatorBands);
 
-    outputBands.resize(numBands_);
+    juce::AudioBuffer<float> tempOut(carrier.getNumChannels(), carrier.getNumSamples());
+
+    tempOut.clear();
+    
+    for (auto& band : carrierBands) {
+        for (int channel = 0; channel < carrier.getNumChannels(); ++channel) {
+            const float* car = band.getReadPointer(channel);
+            float* out = tempOut.getWritePointer(channel);
+            for (int sample = 0; sample < carrier.getNumSamples(); ++sample) {
+                out[sample] += car[sample];
+            }
+        }
+    }
+
+    output = tempOut;
+
+    /*filterBank_.process(modulator, modulatorBands);*/
+
+    /*outputBands.resize(numBands_);
     for (int i = 0; i < numBands_; ++i) {
         outputBands[i].setSize(carrier.getNumChannels(), carrier.getNumSamples(), false, false, true);
         modulator_.applyModulation(carrierBands[i], modulatorBands[i], outputBands[i]);
     }
 
-    mixOutput(output);
+    mixOutput(output);*/
 }
 
 void Vocoder::mixOutput(juce::AudioBuffer<float>& output)
@@ -34,6 +51,7 @@ void Vocoder::mixOutput(juce::AudioBuffer<float>& output)
             for (int sample = 0; sample < output.getNumSamples(); ++sample) {
                 out[sample] += bandData[sample];
             }
+
         }
     }
 }
